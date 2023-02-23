1d877f65fee57d60804cfb405768a399a14ed4d8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-23 16:43:12 -0300

    Create Padrino.java

---
+++ src/main/java/com/example/gatosspringboot/model/Padrino.java
@@ -0,0 +1,27 @@
+package com.example.gatosspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="padrinos")
+@PrimaryKeyJoinColumn(referencedColumnName = "dni")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Padrino extends Persona implements Serializable {
+
+    @OneToMany(mappedBy = "padrino", fetch = FetchType.LAZY)
+    private List<Gato> listaGatos;
+
+    @OneToMany(mappedBy = "padrino", fetch = FetchType.LAZY)
+    private List<Cuota> listaCuotas;
+
+}
