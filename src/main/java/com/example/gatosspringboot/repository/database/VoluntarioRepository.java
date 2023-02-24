5895db218d2a7882c3dddc861ed48752b3666ae5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-02-23 21:26:34 -0300

    Create VoluntarioRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/VoluntarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Voluntario;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface VoluntarioRepository extends CrudRepository<Voluntario, Long> {
+    @Query("from Voluntario v where v.dni= ?1")
+    Optional<Voluntario> findByDni(String dni);
+}
