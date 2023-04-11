09e5af92217ed7990d05e9f07db2363634ba4df3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-11 17:04:33 -0300

    Update EstadoService.java

--- src/main/java/com/example/gatosspringboot/service/imple/EstadoService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/EstadoService.java
@@ -1,5 +1,6 @@
 package com.example.gatosspringboot.service.imple;
 
+import com.example.gatosspringboot.exception.NonExistingException;
 import com.example.gatosspringboot.model.Estado;
 import com.example.gatosspringboot.model.EstadoNombre;
 import com.example.gatosspringboot.repository.database.EstadoRepository;
@@ -7,33 +8,49 @@ import com.example.gatosspringboot.service.interfaces.IEstadoService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class EstadoService implements IEstadoService {
 
-    private EstadoRepository repo;
+    private final EstadoRepository repo;
 
-    @Override
-    public Estado estadoPendiente(Estado estado) {
-        LocalDate fecha=LocalDate.now();
-        estado.setFecha(fecha);
-        estado.setEstado(EstadoNombre.PENDIENTE);
-        return this.repo.save(estado);
+    public EstadoService(EstadoRepository repo) {
+        this.repo = repo;
     }
 
     @Override
-    public Estado estadoAprobado(Estado estado, Long id) {
+    public Estado crearPendiente() {
+        Estado pendiente=new Estado();
         LocalDate fecha=LocalDate.now();
-        estado.setFecha(fecha);
-        estado.setEstado(EstadoNombre.APROBADA);
-        return this.repo.save(estado);
+        pendiente.setFecha(fecha);
+        pendiente.setEstado(EstadoNombre.PENDIENTE);
+        return this.repo.save(pendiente);
     }
 
     @Override
-    public Estado estadoRechazado(Estado estado, Long id) {
+    public Estado estadoAprobado(Long id) {
+        Estado aprobado=this.buscarById(id);
         LocalDate fecha=LocalDate.now();
-        estado.setFecha(fecha);
-        estado.setEstado(EstadoNombre.RECHAZADA);
-        return this.repo.save(estado);
+        aprobado.setFecha(fecha);
+        aprobado.setEstado(EstadoNombre.APROBADA);
+        return this.repo.save(aprobado);
+    }
+
+    @Override
+    public Estado estadoRechazado(Long id) {
+        Estado rechazado=this.buscarById(id);
+        LocalDate fecha=LocalDate.now();
+        rechazado.setFecha(fecha);
+        rechazado.setEstado(EstadoNombre.RECHAZADA);
+        return this.repo.save(rechazado);
+    }
+
+    private Estado buscarById(Long id){
+        Optional<Estado> oEstado=this.repo.findById(id);
+        if(oEstado.isEmpty()){
+            throw new NonExistingException("El estado no existe");
+        }
+        return oEstado.get();
     }
 }
