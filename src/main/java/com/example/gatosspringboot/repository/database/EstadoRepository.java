d1d60428fd9b78189d04c333c3f0b2b268fedfec
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-11 15:31:47 -0300

    Create EstadoRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Estado;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstadoRepository extends CrudRepository<Estado,Long> {
+}
