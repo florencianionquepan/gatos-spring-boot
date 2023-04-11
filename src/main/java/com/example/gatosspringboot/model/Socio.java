f98de66f8e30feb792b756b63bfdf1c9672530d0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-11 11:19:38 -0300

    Create Socio.java

---
+++ src/main/java/com/example/gatosspringboot/model/Socio.java
@@ -0,0 +1,30 @@
+package com.example.gatosspringboot.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name="socios")
+@PrimaryKeyJoinColumn(referencedColumnName = "id")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Socio extends Persona implements Serializable {
+
+    @OneToMany
+    @JsonIgnoreProperties(value="socio")
+    private List<Aspirante> aspirantes;
+
+    @OneToOne
+    @JoinColumn(name="us_id")
+    private Usuario usuario;
+
+}
