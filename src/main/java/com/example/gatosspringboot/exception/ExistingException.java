13295cf4ef6ea670da0d45fff603547a16427530
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-11 11:43:16 -0300

    Create ExistingException.java

---
+++ src/main/java/com/example/gatosspringboot/exception/ExistingException.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.exception;
+
+public class ExistingException extends RuntimeException{
+    public ExistingException(String message) {
+        super(message);
+    }
+}
