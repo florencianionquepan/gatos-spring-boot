072c4ea32f6c4d450aeba318e59b378817a053b7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-11 17:04:45 -0300

    Update IEstadoService.java

--- src/main/java/com/example/gatosspringboot/service/interfaces/IEstadoService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IEstadoService.java
@@ -3,7 +3,7 @@ package com.example.gatosspringboot.service.interfaces;
 import com.example.gatosspringboot.model.Estado;
 
 public interface IEstadoService {
-    Estado estadoPendiente(Estado estado);
-    Estado estadoAprobado(Estado estado, Long id);
-    Estado estadoRechazado(Estado estado, Long id);
+    Estado crearPendiente();
+    Estado estadoAprobado(Long id);
+    Estado estadoRechazado(Long id);
 }
