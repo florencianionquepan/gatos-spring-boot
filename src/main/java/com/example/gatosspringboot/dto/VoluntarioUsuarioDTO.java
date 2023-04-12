eb121f5ea0e0d6a6c0bdc30405995ae74392ea33
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-12 12:10:34 -0300

    Create VoluntarioUsuarioDTO.java

---
+++ src/main/java/com/example/gatosspringboot/dto/VoluntarioUsuarioDTO.java
@@ -0,0 +1,17 @@
+package com.example.gatosspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.Valid;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoluntarioUsuarioDTO {
+    @Valid
+    private UsuarioRespDTO usuario;
+}
