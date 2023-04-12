2d97100bd2ba2f4a6029ba91135716e8d7983506
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-04-12 12:09:39 -0300

    Update UsuarioRespDTO.java

--- src/main/java/com/example/gatosspringboot/dto/UsuarioRespDTO.java
+++ src/main/java/com/example/gatosspringboot/dto/UsuarioRespDTO.java
@@ -2,11 +2,16 @@ package com.example.gatosspringboot.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
 public class UsuarioRespDTO {
+    @NotNull
+    @Email
     private String mail;
 }
