d317f331ab815f7ab9dc3b5066a93b49c8a10b06
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-13 16:35:09 -0300

    Create IPersonaMapper.java

---
+++ src/main/java/com/example/gatosspringboot/dto/mapper/IPersonaMapper.java
@@ -0,0 +1,15 @@
+package com.example.gatosspringboot.dto.mapper;
+
+import com.example.gatosspringboot.dto.PersonaDTO;
+import com.example.gatosspringboot.model.Persona;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel="spring")
+public interface IPersonaMapper {
+    Persona mapToPersona(PersonaDTO dto);
+    PersonaDTO mapToEntity(Persona entity);
+    List<Persona> mapToListDto(List<Persona> entities);
+}
