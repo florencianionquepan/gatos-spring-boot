a832859e2b185ba1d226ffbd73cdb2d9f35df39b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-13 20:11:52 -0300

    Create SecurityConfig.java
    Implementa authentication y authorization

---
+++ src/main/java/com/example/gatosspringboot/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.example.gatosspringboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    //authentication
+    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+        UserDetails admin= User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user= User.withUsername("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin,user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/gatos").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/voluntarios").authenticated()
+                .and().formLogin().and().build();
+    }
+}
