bcf2307243e816e3bc5ce12e3f8bb1c5fc630c07
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-14 00:42:52 -0300

    Create RolRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/RolRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Rol;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RolRepository extends CrudRepository<Rol,Integer> {
+}
