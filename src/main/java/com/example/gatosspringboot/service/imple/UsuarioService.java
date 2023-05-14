9c457888e66d81fa7fdbca6c81b71bacf6b2be07
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-14 00:43:02 -0300

    Update UsuarioService.java

--- src/main/java/com/example/gatosspringboot/service/imple/UsuarioService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/UsuarioService.java
@@ -3,6 +3,7 @@ package com.example.gatosspringboot.service.imple;
 import com.example.gatosspringboot.exception.ExistingException;
 import com.example.gatosspringboot.model.Rol;
 import com.example.gatosspringboot.model.Usuario;
+import com.example.gatosspringboot.repository.database.RolRepository;
 import com.example.gatosspringboot.repository.database.UsuarioRepository;
 import com.example.gatosspringboot.service.interfaces.IUsuarioService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,11 +19,14 @@ public class UsuarioService implements IUsuarioService {
 
     private final UsuarioRepository usRepo;
     private final PasswordEncoder passwordEncoder;
+    private final RolRepository rolRepo;
 
     public UsuarioService(UsuarioRepository usRepo,
-                          PasswordEncoder passwordEncoder) {
+                          PasswordEncoder passwordEncoder,
+                          RolRepository rolRepo) {
         this.usRepo = usRepo;
         this.passwordEncoder = passwordEncoder;
+        this.rolRepo = rolRepo;
     }
 
     @Override
@@ -34,10 +38,9 @@ public class UsuarioService implements IUsuarioService {
     //metodo para crear usuario con email + contraseña rol USER
     public String altaUsuarioCompleto(Usuario us) {
         this.existeEmail(us.getMail());
+        //traigo role_user
         List<Rol> roles=new ArrayList<Rol>(
-                Arrays.asList(
-                        new Rol(1,"USER",new ArrayList<Usuario>())
-                        )
+                List.of(this.rolRepo.findById(1).get())
         );
         us.setRoles(roles);
         us.setContrasenia(passwordEncoder.encode(us.getContrasenia()));
