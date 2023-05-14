07fa9192875c7e8f8219ae00df7836a722ecdbb5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-14 00:01:05 -0300

    Agrega métodos en usuario service
    Se encripta password en servicio.
    Se chequea que el email no este repetido.
    Se deja altaUsuarioEmail para implementar luego.

--- src/main/java/com/example/gatosspringboot/service/imple/UsuarioService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/UsuarioService.java
@@ -1,22 +1,28 @@
 package com.example.gatosspringboot.service.imple;
 
+import com.example.gatosspringboot.exception.ExistingException;
 import com.example.gatosspringboot.model.Rol;
 import com.example.gatosspringboot.model.Usuario;
 import com.example.gatosspringboot.repository.database.UsuarioRepository;
 import com.example.gatosspringboot.service.interfaces.IUsuarioService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService implements IUsuarioService {
 
     private final UsuarioRepository usRepo;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsuarioService(UsuarioRepository usRepo) {
+    public UsuarioService(UsuarioRepository usRepo,
+                          PasswordEncoder passwordEncoder) {
         this.usRepo = usRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -25,13 +31,33 @@ public class UsuarioService implements IUsuarioService {
     }
 
     @Override
-    public Usuario altaUsuario(Usuario us) {
+    //metodo para crear usuario con email + contraseña rol USER
+    public String altaUsuarioCompleto(Usuario us) {
+        this.existeEmail(us.getMail());
         List<Rol> roles=new ArrayList<Rol>(
                 Arrays.asList(
-                        new Rol(1,"user",new ArrayList<Usuario>())
+                        new Rol(1,"USER",new ArrayList<Usuario>())
                         )
         );
         us.setRoles(roles);
-        return this.usRepo.save(us);
+        us.setContrasenia(passwordEncoder.encode(us.getContrasenia()));
+        this.usRepo.save(us);
+        return us.getMail();
     }
+
+    @Override
+    public String altaUsuarioEmail(String email) {
+        return null;
+    }
+
+    //No se admiten emails repetidos
+    private void existeEmail(String email){
+        Optional<Usuario> oUsuario=this.usRepo.findByEmail(email);
+        if(oUsuario.isPresent()){
+            throw new ExistingException
+                    (String.format("El email %d ya se encuentra registrado",email)
+                    );
+        }
+    }
+
 }
