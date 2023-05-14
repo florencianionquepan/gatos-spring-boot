9c96562e8adac786541a955cf317b3870c835b5f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-14 00:46:13 -0300

    Update Estado.java

--- src/main/java/com/example/gatosspringboot/model/Estado.java
+++ src/main/java/com/example/gatosspringboot/model/Estado.java
@@ -19,7 +19,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Estado implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
     private LocalDate fecha;
