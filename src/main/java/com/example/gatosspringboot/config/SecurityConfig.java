82186a71b122e6514008964b73135c45cd143c5e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-13 21:14:52 -0300

    obtengo usuario de UsuarioUserDetailsService()

--- src/main/java/com/example/gatosspringboot/config/SecurityConfig.java
+++ src/main/java/com/example/gatosspringboot/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.gatosspringboot.config;
 
+import com.example.gatosspringboot.service.imple.UsuarioUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -21,7 +22,7 @@ public class SecurityConfig {
     @Bean
     //authentication
     public UserDetailsService userDetailsService(PasswordEncoder encoder){
-        UserDetails admin= User.withUsername("admin")
+        /*UserDetails admin= User.withUsername("admin")
                 .password(encoder.encode("admin"))
                 .roles("ADMIN")
                 .build();
@@ -30,7 +31,8 @@ public class SecurityConfig {
                 .password(encoder.encode("user"))
                 .roles("USER")
                 .build();
-        return new InMemoryUserDetailsManager(admin,user);
+        return new InMemoryUserDetailsManager(admin,user);*/
+        return new UsuarioUserDetailsService();
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
