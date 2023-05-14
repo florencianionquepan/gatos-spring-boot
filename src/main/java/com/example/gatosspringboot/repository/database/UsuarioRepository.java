c6b88d82e0dd148144d5f35e0dcfa7a7e4608b7c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-13 21:17:01 -0300

    obtiene Optional<Usuario> con email

--- src/main/java/com/example/gatosspringboot/repository/database/UsuarioRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/UsuarioRepository.java
@@ -1,8 +1,13 @@
 package com.example.gatosspringboot.repository.database;
 
 import com.example.gatosspringboot.model.Usuario;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario,Long> {
 
+    @Query("from Usuario u where u.email= ?1")
+    Optional<Usuario> findByEmail(String email);
 }
