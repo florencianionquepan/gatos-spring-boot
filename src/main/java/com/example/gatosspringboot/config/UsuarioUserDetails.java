c1c5ee5cb9cacb22fcc1fd81842ea6c1404673bb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-05-13 21:17:29 -0300

    Convierte Usuario en UsuarioUserDetails

---
+++ src/main/java/com/example/gatosspringboot/config/UsuarioUserDetails.java
@@ -0,0 +1,61 @@
+package com.example.gatosspringboot.config;
+
+import com.example.gatosspringboot.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsuarioUserDetails implements UserDetails {
+
+    private String email;
+    private String contrasenia;
+    private List<GrantedAuthority> authorities;
+
+    public UsuarioUserDetails(Usuario user) {
+        email=user.getMail();
+        contrasenia=user.getContrasenia();
+        authorities= user.getRoles().stream()
+                .map(role->new SimpleGrantedAuthority(role.getNombre()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return contrasenia;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
