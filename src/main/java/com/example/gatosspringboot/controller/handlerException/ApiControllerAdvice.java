4905e0bacdc534b2720696a7f163d561e3cec5ea
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-02 21:04:53 -0300

    agrego la excepción EXistingException

--- src/main/java/com/example/gatosspringboot/controller/handlerException/ApiControllerAdvice.java
+++ src/main/java/com/example/gatosspringboot/controller/handlerException/ApiControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.example.gatosspringboot.controller.handlerException;
 
 import com.example.gatosspringboot.dto.response.ExceptionDTO;
+import com.example.gatosspringboot.exception.ExistingException;
 import com.example.gatosspringboot.exception.NonExistingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -28,6 +29,13 @@ public class ApiControllerAdvice {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionDTO doesntExist(NonExistingException ex){
+        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),null);
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionDTO doesntExist(ExistingException ex){
         return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),null);
     }
 
