c0d9314220b2b9f661ab769b5fed473a9a1a0fb5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-04 16:41:00 -0300

    Create SolicitudController.java

---
+++ src/main/java/com/example/gatosspringboot/controller/SolicitudController.java
@@ -0,0 +1,9 @@
+package com.example.gatosspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/solicitudes")
+public class SolicitudController {
+}
