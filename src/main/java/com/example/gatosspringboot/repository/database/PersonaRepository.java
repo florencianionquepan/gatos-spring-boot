c028f8ace7274a0d22e1dc7a0dae41c62555ae17
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-04 15:10:22 -0300

    Create PersonaRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/PersonaRepository.java
@@ -0,0 +1,12 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Persona;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface PersonaRepository extends CrudRepository<Persona,Long> {
+    @Query("from Persona p where p.dni= ?1")
+    Optional<Persona> findByDni(String dni);
+}
