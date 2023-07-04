636a72f3aeceb448408040c8b6e6d57f5296c419
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-04 18:27:53 -0300

    agrega post a solicitud controller

--- src/main/java/com/example/gatosspringboot/controller/SolicitudController.java
+++ src/main/java/com/example/gatosspringboot/controller/SolicitudController.java
@@ -1,9 +1,53 @@
 package com.example.gatosspringboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gatosspringboot.dto.SolicitudDTO;
+import com.example.gatosspringboot.dto.mapper.ISolicitudMapper;
+import com.example.gatosspringboot.model.Solicitud;
+import com.example.gatosspringboot.service.interfaces.ISolicitudService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/solicitudes")
 public class SolicitudController {
+
+    private final ISolicitudService service;
+    private final ISolicitudMapper mapper;
+
+    public SolicitudController(ISolicitudService service,
+                               ISolicitudMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
+    private ResponseEntity<?> successResponse(List<?> lista){
+        mensajeBody.put("Success",Boolean.TRUE);
+        mensajeBody.put("data",lista);
+        return ResponseEntity.ok(mensajeBody);
+    }
+
+    private ResponseEntity<?> notSuccessResponse(String mensaje,int id){
+        mensajeBody.put("Success",Boolean.FALSE);
+        mensajeBody.put("data", String.format(mensaje,id));
+        return ResponseEntity
+                .badRequest()
+                .body(mensajeBody);
+    }
+
+    @PostMapping
+    private ResponseEntity<?> nuevaSolicitud(@RequestBody @Valid SolicitudDTO dto){
+        Solicitud nueva=this.mapper.mapToEntity(dto);
+        Solicitud creada=this.service.altaSolicitud(nueva);
+        SolicitudDTO dtoCreado=this.mapper.mapToDto(creada);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dtoCreado);
+    }
+
 }
