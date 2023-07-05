0117237ecd0ab5c387c5d7aaba4e74004ea01d2c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-05 17:15:04 -0300

    consulta verRangoFechas

--- src/main/java/com/example/gatosspringboot/repository/database/SolicitudRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/SolicitudRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+
 @Repository
 public interface SolicitudRepository extends CrudRepository<Solicitud,Long> {
 
@@ -19,4 +21,6 @@ public interface SolicitudRepository extends CrudRepository<Solicitud,Long> {
     @Query("SELECT s FROM Solicitud s WHERE s.gato.id = ?1")
     List<Solicitud> findByGato(Long id);
 
+    @Query("select s from Solicitud s where s.fechaSolicitud>=?1 and s.fechaSolicitud<=?2")
+    List<Solicitud> findBetweenDates(LocalDate desde, LocalDate hasta);
 }
