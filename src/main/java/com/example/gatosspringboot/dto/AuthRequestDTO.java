0913f56a15ff0c382e0a5a653a4fd55d1b0c3d21
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-05 20:15:55 -0300

    Create AuthRequestDTO.java

---
+++ src/main/java/com/example/gatosspringboot/dto/AuthRequestDTO.java
@@ -0,0 +1,18 @@
+package com.example.gatosspringboot.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequestDTO {
+    @NotNull
+    @Email
+    String mail;
+    @NotNull
+    String password;
+}
