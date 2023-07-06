9768741f159dfc5a1a5cab0727c993086062f972
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-05 21:15:08 -0300

    implementa el filtro para validar que el token que se envia por el header sea valido y corresponda a ese usuario

---
+++ src/main/java/com/example/gatosspringboot/filter/JwtAuthFilter.java
@@ -0,0 +1,51 @@
+package com.example.gatosspringboot.filter;
+
+import com.example.gatosspringboot.service.imple.UsuarioUserDetailsService;
+import com.example.gatosspringboot.service.interfaces.IJwtService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final IJwtService jwtService;
+    private final UsuarioUserDetailsService userDetailsService;
+
+    public JwtAuthFilter(IJwtService jwtService,
+                         UsuarioUserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader=request.getHeader("Authorization");
+        String token=null;
+        String mail=null;
+        if(authHeader!=null && authHeader.startsWith("Bearer ")){
+            token=authHeader.substring(7);
+            mail=jwtService.extractUsername(token);
+        }
+        if(mail!=null && SecurityContextHolder.getContext().getAuthentication()==null){
+            //creo el usuario para validar el token:
+            UserDetails userDetails=userDetailsService.loadUserByUsername(mail);
+            if(jwtService.validateToken(token,userDetails)){
+                UsernamePasswordAuthenticationToken authToken=
+                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}
--- src/main/java/com/example/gatosspringboot/service/interfaces/IJwtService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IJwtService.java
@@ -1,5 +1,9 @@
 package com.example.gatosspringboot.service.interfaces;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface IJwtService {
     String generateToken(String mail);
+    String extractUsername(String token);
+    Boolean validateToken(String token, UserDetails userDetails);
 }
