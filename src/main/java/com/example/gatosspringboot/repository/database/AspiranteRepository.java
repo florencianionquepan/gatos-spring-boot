94025a41d84efc25bf19dcf1afd1ab3a6b1bb29e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 17:33:59 -0300

    Create AspiranteRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/AspiranteRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Aspirante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AspiranteRepository extends CrudRepository<Aspirante,Long> {
+}
