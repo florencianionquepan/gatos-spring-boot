5f0608fcb01e8acae6ad8728956fba37a2734796
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 17:34:03 -0300

    Create AspiranteService.java

---
+++ src/main/java/com/example/gatosspringboot/service/imple/AspiranteService.java
@@ -0,0 +1,61 @@
+package com.example.gatosspringboot.service.imple;
+
+import com.example.gatosspringboot.model.Aspirante;
+import com.example.gatosspringboot.model.Estado;
+import com.example.gatosspringboot.model.Socio;
+import com.example.gatosspringboot.repository.database.AspiranteRepository;
+import com.example.gatosspringboot.service.interfaces.IAspiranteService;
+import com.example.gatosspringboot.service.interfaces.IEstadoService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AspiranteService implements IAspiranteService {
+
+    private final AspiranteRepository repo;
+    private final IEstadoService estadoService;
+
+    public AspiranteService(AspiranteRepository repo,
+                            IEstadoService estadoService) {
+        this.repo = repo;
+        this.estadoService = estadoService;
+    }
+
+    @Override
+    public Aspirante altaAspirante(Aspirante aspirante) {
+        Estado pendiente=this.estadoService.crearPendiente();
+        aspirante.setEstados(new ArrayList<>(Arrays.asList(pendiente)));
+        return this.repo.save(aspirante);
+    }
+
+    @Override
+    //aca debo traer el socio que lo acepto
+    //se crea el voluntario/transito
+    public Aspirante aceptarAspirante(Aspirante aspirante) {
+        return null;
+    }
+
+    @Override
+    //aca debo traer el socio que lo rechazo
+    public Aspirante rechazarAspirante(Aspirante aspirante) {
+        return null;
+    }
+
+    @Override
+    public List<Aspirante> listarTodos() {
+        return null;
+    }
+
+    @Override
+    public List<Aspirante> listarByEstado(Estado estado) {
+        return null;
+    }
+
+    @Override
+    public List<Aspirante> listarBySocio(Socio socio) {
+        return null;
+    }
+}
