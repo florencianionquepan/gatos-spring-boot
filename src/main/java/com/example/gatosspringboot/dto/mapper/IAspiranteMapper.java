9d73f0e8f89ee5a55714c1b7b0d424331d640e99
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 17:32:18 -0300

    Update IAspiranteMapper.java

--- src/main/java/com/example/gatosspringboot/dto/mapper/IAspiranteMapper.java
+++ src/main/java/com/example/gatosspringboot/dto/mapper/IAspiranteMapper.java
@@ -3,11 +3,10 @@ package com.example.gatosspringboot.dto.mapper;
 import com.example.gatosspringboot.dto.AspiranteDTO;
 import com.example.gatosspringboot.model.Aspirante;
 
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
 
 public interface IAspiranteMapper {
     Aspirante mapToEntity(AspiranteDTO dto);
     AspiranteDTO mapToDto(Aspirante entity);
-    List<AspiranteDTO> mapToListDto(List<Entity> entities);
+    List<AspiranteDTO> mapToListDto(List<Aspirante> entities);
 }
