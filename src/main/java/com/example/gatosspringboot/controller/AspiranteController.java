c4773fa117427b1528b15eedae571baf9ba1bfc8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 20:22:11 -0300

    Update AspiranteController.java

--- src/main/java/com/example/gatosspringboot/controller/AspiranteController.java
+++ src/main/java/com/example/gatosspringboot/controller/AspiranteController.java
@@ -2,11 +2,13 @@ package com.example.gatosspringboot.controller;
 
 import com.example.gatosspringboot.dto.AspiranteDTO;
 import com.example.gatosspringboot.dto.mapper.IAspiranteMapper;
+import com.example.gatosspringboot.dto.validator.PutValidationGroup;
 import com.example.gatosspringboot.model.Aspirante;
 import com.example.gatosspringboot.service.interfaces.IAspiranteService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -27,9 +29,9 @@ public class AspiranteController {
         this.mapper = mapper;
     }
 
-    private ResponseEntity<?> successResponse(List<?> lista){
+    private ResponseEntity<?> successResponse(Object data){
         mensajeBody.put("Success",Boolean.TRUE);
-        mensajeBody.put("data",lista);
+        mensajeBody.put("data",data);
         return ResponseEntity.ok(mensajeBody);
     }
 
@@ -52,5 +54,20 @@ public class AspiranteController {
     public ResponseEntity<?> verTodas(){
         List<AspiranteDTO> dtos=this.mapper.mapToListDto(this.service.listarTodos());
         return successResponse(dtos);
+    }
+    @PutMapping("/{id}/estados/aceptada")
+    public ResponseEntity<?> aceptarAspirante(@PathVariable Long id,
+                                              @Validated(PutValidationGroup.class) @RequestBody AspiranteDTO dto){
+        Aspirante aceptado=this.service.aceptarAspirante(this.mapper.mapToEntity(dto));
+        AspiranteDTO dtoAceptado=this.mapper.mapToDto(aceptado);
+        return successResponse(dtoAceptado);
+    }
+
+    @PutMapping("/{id}/estados/rechazada")
+    public ResponseEntity<?> rechazarAspirante(@PathVariable Long id,
+                                              @Validated(PutValidationGroup.class) @RequestBody AspiranteDTO dto){
+        Aspirante rechazado=this.service.rechazarAspirante(this.mapper.mapToEntity(dto));
+        AspiranteDTO dtoRechazado=this.mapper.mapToDto(rechazado);
+        return successResponse(rechazado);
     }
 }
