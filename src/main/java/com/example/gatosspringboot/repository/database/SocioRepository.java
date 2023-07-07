40730be1c535e7e55679d71554b917fddbb4b647
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 20:42:37 -0300

    Create SocioRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/SocioRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Socio;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SocioRepository extends CrudRepository<Socio,Long> {
+}
