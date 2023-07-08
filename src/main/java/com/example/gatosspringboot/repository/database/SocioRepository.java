c92786fb975898607344fc49c926cbbfa8793cc4
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-07 21:49:26 -0300

    Update SocioRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/SocioRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/SocioRepository.java
@@ -1,7 +1,12 @@
 package com.example.gatosspringboot.repository.database;
 
 import com.example.gatosspringboot.model.Socio;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface SocioRepository extends CrudRepository<Socio,Long> {
+    @Query("from Socio s where s.email= ?1")
+    Optional<Socio> findByEmail(String email);
 }
