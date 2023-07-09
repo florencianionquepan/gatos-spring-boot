4aa2637ccfb1ed28cd986d341951be0cb1b5614f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-09 16:20:55 -0300

    ajustes
    El voluntario debe contener el id para hacer un save en su subclase solamente en caso de que ya exista como Persona (que lo hara si se crea a partir de una solicitud como aspirante).

--- src/main/java/com/example/gatosspringboot/dto/mapper/IAspiranteVoluntarioMapper.java
+++ src/main/java/com/example/gatosspringboot/dto/mapper/IAspiranteVoluntarioMapper.java
@@ -4,14 +4,10 @@ import com.example.gatosspringboot.model.Aspirante;
 import com.example.gatosspringboot.model.Voluntario;
 import org.mapstruct.*;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL)
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,componentModel="spring")
 public interface IAspiranteVoluntarioMapper {
 
     @InheritInverseConfiguration
     Voluntario mapAspiranteToVoluntario(Aspirante aspi);
 
-    @AfterMapping
-    default void setNullId(@MappingTarget Voluntario voluntario) {
-        voluntario.setId(null);
-    }
 }
