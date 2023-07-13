d86c352a0c5c0569fdbd7beacd71d6bed64a5e1b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-13 14:44:26 -0300

    agrega socio y lista estados en dto

--- src/main/java/com/example/gatosspringboot/dto/SolicitudVoluntariadoDTO.java
+++ src/main/java/com/example/gatosspringboot/dto/SolicitudVoluntariadoDTO.java
@@ -1,7 +1,10 @@
 package com.example.gatosspringboot.dto;
 
 import com.example.gatosspringboot.dto.validator.PostValidationGroup;
+import com.example.gatosspringboot.dto.validator.PutValidationGroup;
 import com.example.gatosspringboot.dto.validator.ValueOfEnum;
+import com.example.gatosspringboot.model.Estado;
+import com.example.gatosspringboot.model.Socio;
 import com.example.gatosspringboot.model.TipoVoluntariado;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -10,16 +13,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SolicitudVoluntariadoDTO {
     private Long id;
+
     @Valid
     private PersonaDTO aspirante;
+
     @NotNull
     @ValueOfEnum(enumClass=TipoVoluntariado.class, groups = PostValidationGroup.class,
             message = "Debe ser de tipo 'VOLUNTARIO' o 'TRANSITO'.")
     private String voluntariado;
+
+    private List<String> estados;
+
+    @NotNull(groups = PutValidationGroup.class)
+    private Socio socio;
 }
