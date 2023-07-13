719ffad8f50893a905bfad2efd854b91843e3d1e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-13 00:59:27 -0300

    Update SolicitudVoluntariadoController.java

--- src/main/java/com/example/gatosspringboot/controller/SolicitudVoluntariadoController.java
+++ src/main/java/com/example/gatosspringboot/controller/SolicitudVoluntariadoController.java
@@ -1,9 +1,11 @@
 package com.example.gatosspringboot.controller;
 
+import com.example.gatosspringboot.model.SolicitudVoluntariado;
 import com.example.gatosspringboot.service.interfaces.ISolicitudVoluntariadoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -34,4 +36,21 @@ public class SolicitudVoluntariadoController {
                 .badRequest()
                 .body(mensajeBody);
     }
+
+    @PostMapping
+    public ResponseEntity<?> nueva(@RequestBody SolicitudVoluntariado solicitud){
+        SolicitudVoluntariado creada=this.service.nueva(solicitud);
+        return ResponseEntity.status(HttpStatus.CREATED).body(creada);
+    }
+
+    @PutMapping("/{id}/estados/aceptada")
+    public ResponseEntity<?> aceptarSolicitud(){
+        return null;
+    }
+
+    @PutMapping("/{id}/estados/rechazada")
+    public ResponseEntity<?> rechazarSolicitud(){
+        return null;
+    }
+
 }
