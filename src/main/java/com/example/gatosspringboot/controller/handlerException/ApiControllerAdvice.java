cefe7888758b57b9a354474682dc1f0ef21533e3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-18 15:38:39 -0300

    agrega exceptions en api controller advice

--- src/main/java/com/example/gatosspringboot/controller/handlerException/ApiControllerAdvice.java
+++ src/main/java/com/example/gatosspringboot/controller/handlerException/ApiControllerAdvice.java
@@ -1,9 +1,14 @@
 package com.example.gatosspringboot.controller.handlerException;
 
 import com.example.gatosspringboot.dto.response.ExceptionDTO;
+import com.example.gatosspringboot.dto.response.SimpleExceptionDTO;
 import com.example.gatosspringboot.exception.ExistingException;
 import com.example.gatosspringboot.exception.NonExistingException;
+import com.example.gatosspringboot.exception.PersonNotFound;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,8 @@ import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class ApiControllerAdvice {
+
+    private Logger logger= LoggerFactory.getLogger(ApiControllerAdvice.class);
 
     @ExceptionHandler
     @ResponseBody
@@ -39,12 +46,19 @@ public class ApiControllerAdvice {
         return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(),null);
     }
 
-    @ExceptionHandler(IllegalArgumentException.class)
+    @ExceptionHandler(PersonNotFound.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public SimpleExceptionDTO notFound(PersonNotFound ex){
+        return new SimpleExceptionDTO(HttpStatus.NOT_FOUND.value(),ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ExceptionDTO handleIllegalArgumentException(IllegalArgumentException ex) {
-        String errorMessage = "El estado proporcionado no es válido.";
-        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), errorMessage, null);
+    public ExceptionDTO handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        //logger.info("excepcion: "+ex.getMessage());
+        return new ExceptionDTO(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null);
     }
 
 }
