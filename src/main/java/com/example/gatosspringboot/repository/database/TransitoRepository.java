71617a9db6ecdaa4572e96b767be0d8a0759ea33
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-19 14:31:04 -0300

    Create TransitoRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/TransitoRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Transito;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TransitoRepository extends CrudRepository<Transito,Long> {
+}
