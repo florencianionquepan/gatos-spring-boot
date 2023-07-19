a76c555052ce8fee83e3054f8ece0f32317fb9bb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-19 15:51:12 -0300

    se implementa lógica de nuevo transito y de métodos listar

--- src/main/java/com/example/gatosspringboot/service/imple/TransitoService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/TransitoService.java
@@ -1,31 +1,51 @@
 package com.example.gatosspringboot.service.imple;
 
+import com.example.gatosspringboot.model.Persona;
 import com.example.gatosspringboot.model.Transito;
+import com.example.gatosspringboot.repository.database.PersonaRepository;
 import com.example.gatosspringboot.repository.database.TransitoRepository;
+import com.example.gatosspringboot.service.interfaces.IPersonaService;
 import com.example.gatosspringboot.service.interfaces.ITransitoService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class TransitoService implements ITransitoService {
 
     public final TransitoRepository repo;
+    public final PersonaRepository persoRepo;
+    public final IPersonaService persoService;
 
-    public TransitoService(TransitoRepository repo) {
+    public TransitoService(TransitoRepository repo,
+                           PersonaRepository persoRepo,
+                           IPersonaService persoService) {
         this.repo = repo;
+        this.persoRepo = persoRepo;
+        this.persoService = persoService;
     }
 
     @Override
     public List<Transito> listarTodos() {
-        return null;
+        return (List<Transito>) this.repo.findAll();
     }
 
     @Override
     public List<Transito> listarByLocalidad(String localidad) {
-        return null;
+        return this.repo.buscarByLocalidad(localidad);
     }
 
     @Override
+    //aca debe traer su id si o si si existe como persona
     public Transito nuevo(Transito transito) {
-        return null;
+        Optional<Persona> oPerso=this.persoRepo.findByDni(transito.getDni());
+        if(oPerso.isPresent()){
+            this.persoService.validarEmailUnico(transito.getEmail());
+            transito.setId(oPerso.get().getId());
+            this.repo.saveTransito(transito.getId());
+            return transito;
+        }
+        return this.repo.save(transito);
     }
 }
