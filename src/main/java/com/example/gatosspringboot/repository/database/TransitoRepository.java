fca4ac6ea1da8663aa55f351de84589ef738bdeb
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-19 15:49:54 -0300

    crea buscarByLocalidad y saveTransito nativa

--- src/main/java/com/example/gatosspringboot/repository/database/TransitoRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/TransitoRepository.java
@@ -1,7 +1,21 @@
 package com.example.gatosspringboot.repository.database;
 
 import com.example.gatosspringboot.model.Transito;
+import com.example.gatosspringboot.model.Voluntario;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TransitoRepository extends CrudRepository<Transito,Long> {
+
+    @Query("from Transito t where t.localidad= ?1")
+    List<Transito> buscarByLocalidad(String localidad);
+
+    @Modifying
+    @Query(nativeQuery = true,
+            value="INSERT INTO transitos (id) VALUES (:id)")
+    void saveTransito(@Param("id") Long id);
 }
