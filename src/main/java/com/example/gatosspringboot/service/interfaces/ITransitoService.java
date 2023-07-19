a9933d09be2a3eaa335add47bde91c8fbdd2f73b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-19 14:31:09 -0300

    Create ITransitoService.java

---
+++ src/main/java/com/example/gatosspringboot/service/interfaces/ITransitoService.java
@@ -0,0 +1,11 @@
+package com.example.gatosspringboot.service.interfaces;
+
+import com.example.gatosspringboot.model.Transito;
+
+import java.util.List;
+
+public interface ITransitoService {
+    List<Transito> listarTodos();
+    List<Transito> listarByLocalidad(String localidad);
+    Transito nuevo(Transito transito);
+}
