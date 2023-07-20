a07787a34e959d97455c176b866404290437a9d0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-20 16:10:56 -0300

    Se devuelve booleano en envío de email

--- src/main/java/com/example/gatosspringboot/service/imple/EmailService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/EmailService.java
@@ -1,6 +1,9 @@
 package com.example.gatosspringboot.service.imple;
 
 import com.example.gatosspringboot.service.interfaces.IEmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,18 +12,25 @@ import org.springframework.stereotype.Service;
 public class EmailService implements IEmailService {
 
     final JavaMailSender emailSender;
+    private Logger logger= LoggerFactory.getLogger(EmailService.class);
 
     public EmailService(JavaMailSender emailSender) {
         this.emailSender = emailSender;
     }
 
     @Override
-    public void sendMessage(String to, String subject, String text) {
-        SimpleMailMessage message=new SimpleMailMessage();
-        message.setFrom("florencianionquepan@gmail.com");
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(text);
-        this.emailSender.send(message);
+    public boolean sendMessage(String to, String subject, String text) {
+        try{
+            SimpleMailMessage message=new SimpleMailMessage();
+            message.setFrom("florencianionquepan@gmail.com");
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            this.emailSender.send(message);
+            return true;
+        }catch (MailException ex){
+            logger.error(ex.getMessage());
+            return false;
+        }
     }
 }
