9ba28e7dfd8e7ad2459118ab98f45f0bd9047a9d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-21 18:21:19 -0300

    Create EstadoDTO.java

---
+++ src/main/java/com/example/gatosspringboot/dto/EstadoDTO.java
@@ -0,0 +1,27 @@
+package com.example.gatosspringboot.dto;
+
+import com.example.gatosspringboot.dto.validator.ValueOfEnum;
+import com.example.gatosspringboot.model.EstadoNombre;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EstadoDTO {
+
+    private LocalDate fecha;
+
+    @NotNull
+    @ValueOfEnum(enumClass= EstadoNombre.class,
+            message = "Debe ser de tipo 'PENDIENTE','APROBADA' o 'RECHAZADA' o 'CERRADA'.")
+    private String estado;
+
+    private String motivo;
+}
