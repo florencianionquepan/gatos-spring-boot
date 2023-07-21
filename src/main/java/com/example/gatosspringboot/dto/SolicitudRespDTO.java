1fe36421b5138e9f7afda1b6e0e860a6fbb0fa53
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-21 18:18:19 -0300

    Create SolicitudRespDTO.java

---
+++ src/main/java/com/example/gatosspringboot/dto/SolicitudRespDTO.java
@@ -0,0 +1,25 @@
+package com.example.gatosspringboot.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SolicitudRespDTO {
+
+    Long id;
+    List<EstadoDTO> estados;
+
+    @JsonIgnoreProperties(value="solicitudes")
+    PersonaDTO solicitante;
+
+    @JsonIgnoreProperties(value="solicitudes")
+    GatoDTO gato;
+}
