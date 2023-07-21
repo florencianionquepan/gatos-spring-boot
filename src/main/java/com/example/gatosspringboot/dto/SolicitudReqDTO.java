ecd74aefd08ef8c6fcfdd0cee891f61bbe08210c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-21 18:18:10 -0300

    modifico nombre a SolicitudReqDTO

--- src/main/java/com/example/gatosspringboot/dto/SolicitudReqDTO.java
+++ src/main/java/com/example/gatosspringboot/dto/SolicitudReqDTO.java
@@ -1,30 +1,26 @@
 package com.example.gatosspringboot.dto;
 
-import com.example.gatosspringboot.model.Estado;
-import com.example.gatosspringboot.model.Gato;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SolicitudDTO {
+public class SolicitudReqDTO {
 
     Long id;
-    List<Estado> estados;
+    List<EstadoDTO> estados;
 
     @Valid
     @JsonIgnoreProperties(value={"solicitudes","gato"})
-    PersonaDTO solicitante;
+    PersonaEmailDTO solicitante;
     @Valid
     GatoIdDTO gato;
 }
