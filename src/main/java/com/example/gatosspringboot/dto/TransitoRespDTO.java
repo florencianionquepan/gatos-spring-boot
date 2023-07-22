9fc7451ffbd9ba7be0b0355bd95eb6fc81c21ad3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-22 20:09:50 -0300

    Update TransitoRespDTO.java

--- src/main/java/com/example/gatosspringboot/dto/TransitoRespDTO.java
+++ src/main/java/com/example/gatosspringboot/dto/TransitoRespDTO.java
@@ -1,4 +1,11 @@
 package com.example.gatosspringboot.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class TransitoRespDTO extends PersonaDTO{
 }
