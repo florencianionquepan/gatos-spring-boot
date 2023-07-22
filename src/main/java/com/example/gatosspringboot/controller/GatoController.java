69e77e09144a1b824e5166b9d6a4a76b63affbbe
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-07-22 16:39:05 -0300

    endpoint para agregar transito a un gato creado

--- src/main/java/com/example/gatosspringboot/controller/GatoController.java
+++ src/main/java/com/example/gatosspringboot/controller/GatoController.java
@@ -2,8 +2,10 @@ package com.example.gatosspringboot.controller;
 
 import com.example.gatosspringboot.dto.FichaDTO;
 import com.example.gatosspringboot.dto.GatoDTO;
+import com.example.gatosspringboot.dto.TransitoIdDTO;
 import com.example.gatosspringboot.dto.mapper.IFichaMapper;
 import com.example.gatosspringboot.dto.mapper.IGatoMapper;
+import com.example.gatosspringboot.dto.mapper.ITransitoIdMapper;
 import com.example.gatosspringboot.model.Gato;
 import com.example.gatosspringboot.service.interfaces.IGatoService;
 import org.springframework.http.HttpStatus;
@@ -22,15 +24,18 @@ public class GatoController {
     private final IGatoService gatoSer;
     private final IGatoMapper mapper;
     private final IFichaMapper fichaMap;
+    private final ITransitoIdMapper transitoMap;
 
     public Map<String,Object> mensajeBody= new HashMap<>();
 
     public GatoController(IGatoService gatoSer,
                           IGatoMapper mapper,
-                          IFichaMapper fichaMap) {
+                          IFichaMapper fichaMap,
+                          ITransitoIdMapper transitoMap) {
         this.gatoSer = gatoSer;
         this.mapper = mapper;
         this.fichaMap = fichaMap;
+        this.transitoMap = transitoMap;
     }
 
     private ResponseEntity<?> successResponse(Object data){
@@ -67,6 +72,14 @@ public class GatoController {
     public ResponseEntity<?> agregarFicha(@RequestBody FichaDTO ficha,
                                           @PathVariable Long id){
         Gato modi=this.gatoSer.agregarFicha(this.fichaMap.mapToEntity(ficha), id);
+        return this.successResponse(this.mapper.mapToDto(modi));
+    }
+
+    @PutMapping("/id/transito")
+    //@PreAuthorize("hasAuthority('ROLE_VOLUNTARIO')")
+    public ResponseEntity<?> agregarTransito(@RequestBody TransitoIdDTO dto,
+                                             @PathVariable Long id){
+        Gato modi=this.gatoSer.agregarTransito(this.transitoMap.mapToEntity(dto), id);
         return this.successResponse(this.mapper.mapToDto(modi));
     }
 
