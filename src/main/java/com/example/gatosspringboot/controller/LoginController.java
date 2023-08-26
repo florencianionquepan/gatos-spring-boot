c2a09581ca6ba6767ab5367dda9beda9cd41768a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-08-26 17:50:02 -0300

    Update LoginController.java

--- src/main/java/com/example/gatosspringboot/controller/LoginController.java
+++ src/main/java/com/example/gatosspringboot/controller/LoginController.java
@@ -4,9 +4,13 @@ import com.example.gatosspringboot.model.Usuario;
 import com.example.gatosspringboot.service.interfaces.IUsuarioService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,14 +19,22 @@ public class LoginController {
     private final IUsuarioService userService;
     private Logger logger= LoggerFactory.getLogger(LoginController.class);
 
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
     public LoginController(IUsuarioService userService) {
         this.userService = userService;
     }
 
-    public Usuario getUserDetailsAfterLogin(Authentication auth){
+    private ResponseEntity<?> successResponse(Object data){
+        mensajeBody.put("success",Boolean.TRUE);
+        mensajeBody.put("data",data);
+        return ResponseEntity.ok(mensajeBody);
+    }
+
+    public ResponseEntity<?> getUserDetailsAfterLogin(Authentication auth){
         logger.info("ingresa al /auth");
         Usuario user=userService.buscarByEmail(auth.getName());
-        return user;
+        return this.successResponse(user);
     }
 
 
