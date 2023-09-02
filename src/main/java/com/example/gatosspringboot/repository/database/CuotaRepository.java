35dbd2a5d0056147519ad6264824078406f79e1b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-02 20:15:24 -0300

    Create CuotaRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/CuotaRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Cuota;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CuotaRepository extends CrudRepository<Cuota,Long> {
+}
