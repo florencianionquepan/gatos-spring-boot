7a6077a6497d67187a2e533a5ec9f326705dce75
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-02 20:15:48 -0300

    Create CuotaController.java

---
+++ src/main/java/com/example/gatosspringboot/controller/CuotaController.java
@@ -0,0 +1,50 @@
+package com.example.gatosspringboot.controller;
+
+import com.example.gatosspringboot.model.Cuota;
+import com.example.gatosspringboot.model.Padrino;
+import com.example.gatosspringboot.service.interfaces.ICuotaService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class CuotaController {
+
+    private final ICuotaService service;
+
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
+    public CuotaController(ICuotaService service) {
+        this.service = service;
+    }
+
+    private ResponseEntity<?> successResponse(Object data){
+        mensajeBody.put("success",Boolean.TRUE);
+        mensajeBody.put("data",data);
+        return ResponseEntity.ok(mensajeBody);
+    }
+
+    private ResponseEntity<?> notSuccessResponse(String mensaje,int id){
+        mensajeBody.put("success",Boolean.FALSE);
+        mensajeBody.put("data", String.format(mensaje,id));
+        return ResponseEntity
+                .badRequest()
+                .body(mensajeBody);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> verByPadrino(Padrino padrino){
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> pagoCuota(@RequestBody Cuota cuota){
+            Cuota cuotaPaga=this.service.nuevaCuota(cuota);
+            return this.successResponse(cuotaPaga);
+    }
+}
