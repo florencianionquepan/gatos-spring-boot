9f166014731266141fd315e081acd62406e04403
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-06 18:52:14 -0300

    Create Notificacion.java

---
+++ src/main/java/com/example/gatosspringboot/model/Notificacion.java
@@ -0,0 +1,33 @@
+package com.example.gatosspringboot.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="notificaciones")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notificacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    private String descripcion;
+    @NotNull
+    private LocalDate fechaCreacion;
+    @NotNull
+    private Boolean leida=false;
+
+    @PrePersist
+    protected void onCreate() {
+        fechaCreacion = LocalDate.now();
+    }
+}
