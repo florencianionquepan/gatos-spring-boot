20d4161708c0d31fd8377c05c1bb085a24475f27
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-06 20:31:20 -0300

    Update VoluntarioRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/VoluntarioRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/VoluntarioRepository.java
@@ -9,14 +9,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface VoluntarioRepository extends CrudRepository<Voluntario, Long> {
-    @Query("from Voluntario v where v.dni= ?1")
+    @Query("from Voluntario v where v.persona.dni= ?1")
     Optional<Voluntario> findByDni(String dni);
 
-    @Query("from Voluntario v where v.email= ?1")
+    @Query("from Voluntario v where v.persona.email= ?1")
     Optional<Voluntario> findByEmail(String email);
-
-    @Modifying
-    @Query(nativeQuery = true,
-            value="INSERT INTO voluntarios (id) VALUES (:id)")
-    void saveVoluntario(@Param("id") Long id);
 }
