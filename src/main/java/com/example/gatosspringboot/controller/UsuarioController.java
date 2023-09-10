ff7ef6fb0ba2e902a31cdad8840d55c05b6c8669
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-10 14:32:00 -0300

    Update UsuarioController.java

--- src/main/java/com/example/gatosspringboot/controller/UsuarioController.java
+++ src/main/java/com/example/gatosspringboot/controller/UsuarioController.java
@@ -1,14 +1,16 @@
 package com.example.gatosspringboot.controller;
 
 import com.example.gatosspringboot.dto.UsuarioPasswordDTO;
-import com.example.gatosspringboot.dto.UsuarioReqDTO;
 import com.example.gatosspringboot.dto.UsuarioEmailDTO;
 import com.example.gatosspringboot.dto.mapper.IUsuarioMapper;
 import com.example.gatosspringboot.dto.mapper.IUsuarioPasswordMapper;
 import com.example.gatosspringboot.model.Usuario;
+import com.example.gatosspringboot.service.imple.UsuarioService;
 import com.example.gatosspringboot.service.interfaces.IUsuarioService;
+import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -22,6 +24,7 @@ public class UsuarioController {
     private final IUsuarioMapper usMap;
     private final IUsuarioPasswordMapper usPasswordMapper;
     public Map<String,Object> mensajeBody= new HashMap<>();
+    private Logger logger= LoggerFactory.getLogger(UsuarioController.class);
 
     private ResponseEntity<?> notSuccessResponse(String mensaje,int id){
         mensajeBody.put("success",Boolean.FALSE);
@@ -59,6 +62,15 @@ public class UsuarioController {
         Usuario user=this.usPasswordMapper.mapToEntity(dto);
         String modiUser=this.usService.modiPassword(user, dto.getPasswordActual());
         return successResponse(modiUser);
+    }
+
+    @GetMapping("/{id}/validacion/{token}")
+    @PermitAll
+    public ResponseEntity<?> validarUsuario(@PathVariable("id") Long id,
+                                            @PathVariable("token") String token){
+        Boolean validado=this.usService.validarUsuario(id,token);
+        return ResponseEntity.ok("Email validado correctamente." +
+                "Ya puede iniciar sesion!");
     }
 
 
