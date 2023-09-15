4c8c1d61440b9dc43c07edfabcfd69aaec825c77
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-14 21:06:32 -0300

    busco gatos segun email de voluntario en repositorio

--- src/main/java/com/example/gatosspringboot/repository/database/GatoRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/GatoRepository.java
@@ -1,7 +1,14 @@
 package com.example.gatosspringboot.repository.database;
 
 import com.example.gatosspringboot.model.Gato;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GatoRepository extends CrudRepository<Gato,Long> {
+
+    @Query("from Gato g WHERE g.voluntario.persona.email = ?1")
+    List<Gato> findByVoluntarioEmail(String email);
+
 }
