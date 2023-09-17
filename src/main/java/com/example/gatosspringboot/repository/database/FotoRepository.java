b1877d4c1dabd00fc110ebc9417219914aff37c7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-17 15:33:18 -0300

    Create FotoRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/FotoRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.Foto;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FotoRepository extends CrudRepository<Foto,Long> {
+}
