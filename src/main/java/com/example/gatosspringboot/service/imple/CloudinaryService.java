677fbdb2a25f0b60fa011ab006fc19dd1c044780
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-17 17:26:14 -0300

    manejo de varias imágenes

--- src/main/java/com/example/gatosspringboot/service/imple/CloudinaryService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/CloudinaryService.java
@@ -2,6 +2,7 @@ package com.example.gatosspringboot.service.imple;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import com.example.gatosspringboot.exception.NonExistingException;
 import com.example.gatosspringboot.service.interfaces.ICloudinaryService;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,7 +10,9 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -20,12 +23,20 @@ public class CloudinaryService implements ICloudinaryService {
 
 
     @Override
-    public Map upload(MultipartFile multipartFile) throws IOException {
-        File file= this.convert(multipartFile);
-        Map result= cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
-        //para no guardar las imagenes en el servidor!!
-        file.delete();
-        return result;
+    public List<Map> upload(MultipartFile[] multipartFiles)  {
+        List<Map> results = new ArrayList<>();
+        try {
+            for (MultipartFile multipartFile : multipartFiles) {
+                File file = this.convert(multipartFile);
+                Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+                //para no guardar las imagenes en el servidor!!
+                file.delete();
+                results.add(result);
+            }
+        } catch (IOException e) {
+            throw new NonExistingException("Ocurrio un error: "+e);
+        }
+        return results;
     }
 
     @Override
