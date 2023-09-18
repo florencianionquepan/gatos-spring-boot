69e666315c3f626da2240279c3e3eadf0f182ff2
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-17 23:08:08 -0300

    se deja este endpoint por si acaso

--- src/main/java/com/example/gatosspringboot/controller/CloudinaryController.java
+++ src/main/java/com/example/gatosspringboot/controller/CloudinaryController.java
@@ -1,5 +1,6 @@
 package com.example.gatosspringboot.controller;
 
+import com.example.gatosspringboot.exception.NonExistingException;
 import com.example.gatosspringboot.service.imple.CloudinaryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -7,7 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -18,14 +24,15 @@ public class CloudinaryController {
     CloudinaryService cloudinaryService;
 
     @PostMapping
-    public ResponseEntity<Map> upload(@RequestParam MultipartFile multipartFile){
-        Map result= null;
-        try {
-            result = cloudinaryService.upload(multipartFile);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+    public ResponseEntity<?> upload(@RequestParam MultipartFile[] multipartFiles) throws IOException {
+        for (MultipartFile multipartFile : multipartFiles){
+            BufferedImage bi= ImageIO.read(multipartFile.getInputStream());
+            if(bi==null){
+                throw new NonExistingException("Alguna imagen no es válida");
+            }
         }
-        return new ResponseEntity<>(result, HttpStatus.OK);
+        List<Map> results = cloudinaryService.upload(multipartFiles);
+        return new ResponseEntity<>(results, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
