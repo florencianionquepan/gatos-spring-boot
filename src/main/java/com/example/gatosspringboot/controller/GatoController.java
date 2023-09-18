1b5aa2d49c4ed9e4d1f3c62759481975535edfb6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-17 23:09:18 -0300

    endpoint que recibe nuevo gato, quedan mejoras para luego

--- src/main/java/com/example/gatosspringboot/controller/GatoController.java
+++ src/main/java/com/example/gatosspringboot/controller/GatoController.java
@@ -5,17 +5,30 @@ import com.example.gatosspringboot.dto.mapper.IFichaMapper;
 import com.example.gatosspringboot.dto.mapper.IGatoMapper;
 import com.example.gatosspringboot.dto.mapper.ITransitoIdMapper;
 import com.example.gatosspringboot.dto.mapper.IVoluntarioEmailMapper;
+import com.example.gatosspringboot.exception.NonExistingException;
 import com.example.gatosspringboot.model.Gato;
 import com.example.gatosspringboot.service.interfaces.IGatoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/gatos")
@@ -75,10 +88,32 @@ public class GatoController {
     }
 
 
-    @PostMapping
-    @PreAuthorize("hasRole('VOLUNTARIO')")
-    public ResponseEntity<?> altaGato(@RequestBody @Valid GatoDTO dto){
-        Gato nuevo=this.gatoSer.altaGato(this.mapper.mapToEntity(dto));
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    //@PreAuthorize("hasRole('VOLUNTARIO')")
+    public ResponseEntity<?> altaGato(@RequestPart String dto,
+                                      @RequestParam MultipartFile[] multipartFiles) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        GatoDTO dtoJson=objectMapper.readValue(dto,GatoDTO.class);
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+        Set<ConstraintViolation<GatoDTO>> violations = validator.validate(dtoJson);
+        if (!violations.isEmpty()) {
+            List<String> errores = violations.stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Error: " + errores);
+        }
+        if (multipartFiles == null || multipartFiles.length == 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("No se proporciono ninguna imagen del gatito");
+        }
+        for (MultipartFile multipartFile : multipartFiles){
+            BufferedImage bi= ImageIO.read(multipartFile.getInputStream());
+            if(bi==null){
+                throw new NonExistingException("Alguna imagen no es válida");
+            }
+        }
+        Gato nuevo=this.gatoSer.altaGato(this.mapper.mapToEntity(dtoJson), multipartFiles);
         mensajeBody.put("Success",Boolean.TRUE);
         mensajeBody.put("data",this.mapper.mapToDto(nuevo));
         return ResponseEntity.status(HttpStatus.CREATED).body(mensajeBody);
