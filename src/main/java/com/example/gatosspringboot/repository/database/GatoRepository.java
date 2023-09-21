fa9ddf76611d4e39e8b05f6b381403165ff8f518
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-21 19:07:05 -0300

    Update GatoRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/GatoRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/GatoRepository.java
@@ -1,5 +1,6 @@
 package com.example.gatosspringboot.repository.database;
 
+import com.example.gatosspringboot.model.Foto;
 import com.example.gatosspringboot.model.Gato;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -11,4 +12,6 @@ public interface GatoRepository extends CrudRepository<Gato,Long> {
     @Query("from Gato g WHERE g.voluntario.persona.email = ?1")
     List<Gato> findByVoluntarioEmail(String email);
 
+    @Query("SELECT g.fotos FROM Gato g WHERE g.id = ?1")
+    List<Foto> findFotosByGatoId(Long gatoId);
 }
