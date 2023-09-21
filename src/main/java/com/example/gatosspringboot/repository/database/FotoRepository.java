0bf7b0f574f495aa00db07316176bb4a930ecab0
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-21 19:07:08 -0300

    Update FotoRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/FotoRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/FotoRepository.java
@@ -3,5 +3,11 @@ package com.example.gatosspringboot.repository.database;
 import com.example.gatosspringboot.model.Foto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface FotoRepository extends CrudRepository<Foto,Long> {
+
+    long deleteByFotoUrl(String fotoUrl);
+
+    List<Foto> findByFotoUrl(String fotoUrl);
 }
