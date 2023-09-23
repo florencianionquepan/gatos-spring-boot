760e4cc480aab4ee25ef83bbd44e27098c7c9976
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-23 19:34:35 -0300

    anotaciones para deserializar json

--- src/main/java/com/example/gatosspringboot/dto/FichaDTO.java
+++ src/main/java/com/example/gatosspringboot/dto/FichaDTO.java
@@ -1,5 +1,10 @@
 package com.example.gatosspringboot.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,11 +16,19 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class FichaDTO {
 
     private Long id;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate ultimaDesparasitacion;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate ultimaTripleFelina;
+    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate ultimaAntirrabica;
     private String comentarios;
+    private String pdf;
 }
