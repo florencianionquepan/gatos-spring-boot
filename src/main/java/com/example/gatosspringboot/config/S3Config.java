2d2d1bbd1d0fbabce9405392331caac94ff7be30
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-23 13:36:09 -0300

    Update S3Config.java

--- src/main/java/com/example/gatosspringboot/config/S3Config.java
+++ src/main/java/com/example/gatosspringboot/config/S3Config.java
@@ -5,33 +5,29 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
 
     @Value("${aws.access_key_id}")
-    private final String awsAccessKey;
+    private String awsAccessKey;
     @Value("${aws.secret_access_key}")
-    private final String awsSecretKey;
+    private String awsSecretKey;
     @Value("${aws.s3.bucket}")
-    private final String awsBucket;
+    private String awsBucket;
     @Value("${aws.s3.region}")
-    private final String awsRegion;
+    private String awsRegion;
 
-    public S3Config(String awsAccessKey, String awsSecretKey, String awsBucket, String awsRegion) {
-        this.awsAccessKey = awsAccessKey;
-        this.awsSecretKey = awsSecretKey;
-        this.awsBucket = awsBucket;
-        this.awsRegion = awsRegion;
-    }
-
+    @Bean
     public AmazonS3 getS3Client(){
         BasicAWSCredentials credentials=new BasicAWSCredentials(awsAccessKey,awsSecretKey);
-        return AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(awsRegion)
-                ).withCredentials(new AWSStaticCredentialsProvider(credentials)).build();
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(awsRegion)
+                .build();
     }
 }
