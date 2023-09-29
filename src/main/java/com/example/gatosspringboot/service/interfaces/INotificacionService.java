1097e79f49dd248935bf626ce866037daf3b01b6
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-29 17:26:43 -0300

    Create INotificacionService.java

---
+++ src/main/java/com/example/gatosspringboot/service/interfaces/INotificacionService.java
@@ -0,0 +1,11 @@
+package com.example.gatosspringboot.service.interfaces;
+
+import com.example.gatosspringboot.model.Gato;
+import com.example.gatosspringboot.model.Notificacion;
+
+import java.util.List;
+
+public interface INotificacionService {
+    Notificacion nuevaSolicitudAdopcion(Gato gato);
+    List<Notificacion> verByPersona(String email);
+}
