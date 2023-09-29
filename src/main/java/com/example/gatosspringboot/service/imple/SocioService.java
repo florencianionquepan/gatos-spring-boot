934184d6d597148109d6f89b0952a81fbb3dfb70
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-09-29 17:26:45 -0300

    Update SocioService.java

--- src/main/java/com/example/gatosspringboot/service/imple/SocioService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/SocioService.java
@@ -38,10 +38,10 @@ public class SocioService implements ISocioService {
 
     @Override
     public Socio buscarByEmailOrException(String email) {
-        Optional<Socio> oSocio=this.repo.findByEmail(email);
-        if(oSocio.isEmpty()){
+        Optional<Socio> oSocio = this.repo.findByEmail(email);
+        if (oSocio.isEmpty()) {
             throw new NonExistingException(
-                    String.format("El socio con email %s no existe",email));
+                    String.format("No existe un socio con email %s", email));
         }
         return oSocio.get();
     }
@@ -51,36 +51,35 @@ public class SocioService implements ISocioService {
     //le dara de alta otro socio
     public Socio altaSocio(Socio socio) {
         this.socioExistente(socio);
-        Optional<Persona> oPersoDni=this.persoRepo.findByDni(socio.getDni());
-        Optional<Usuario> oUser=this.usRepo.findByEmail(socio.getEmail());
+        Optional<Persona> oPersoDni=this.persoRepo.findByDni(socio.getPersona().getDni());
+        Optional<Usuario> oUser=this.usRepo.findByEmail(socio.getPersona().getEmail());
         if(oPersoDni.isEmpty()){
-            this.persoService.validarEmailUnico(socio.getEmail());
-            Usuario admin=this.usService.altaUsuarioSocio(socio.getEmail());
-            socio.setUsuario(admin);
+            this.persoService.validarEmailUnico(socio.getPersona().getEmail());
+            Usuario admin=this.usService.altaUsuarioSocio(socio.getPersona().getEmail());
+            socio.getPersona().setUsuario(admin);
             return this.repo.save(socio);
         }
         //si ya existe como persona
         //si ya tiene el rol admin no se vuelve a agregar
-        Usuario user=oUser.isEmpty()?this.usService.altaUsuarioSocio(socio.getEmail())
+        Usuario user=oUser.isEmpty()?this.usService.altaUsuarioSocio(socio.getPersona().getEmail())
                 :this.usService.agregarRolSocio(oUser.get());
         Persona perso=oPersoDni.get();
-        socio.setId(perso.getId());
-        socio.setUsuario(user);
-        this.repo.saveSocio(perso.getId(),user.getId());
+        socio.getPersona().setUsuario(user);
+        this.repo.save(socio);
         return socio;
     }
 
     //si ya existe con otro dni o email no prosigue-
     private void socioExistente(Socio socio){
-        Optional<Socio> oSocioDni=this.repo.findByDni(socio.getDni());
+        Optional<Socio> oSocioDni=this.repo.findByDni(socio.getPersona().getDni());
         if(oSocioDni.isPresent()){
             throw new ExistingException(
-                    String.format("El socio con dni %s ya existe",socio.getDni()));
+                    String.format("El socio con dni %s ya existe",socio.getPersona().getDni()));
         }
-        Optional<Socio> oSocioEmail=this.repo.findByEmail(socio.getEmail());
+        Optional<Socio> oSocioEmail=this.repo.findByEmail(socio.getPersona().getEmail());
         if(oSocioEmail.isPresent()){
             throw new ExistingException(
-                    String.format("El socio con email %s ya existe",socio.getEmail()));
+                    String.format("El socio con email %s ya existe",socio.getPersona().getEmail()));
         }
     }
 
