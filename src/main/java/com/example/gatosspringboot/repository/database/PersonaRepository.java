073cb44f818ca24d103dc1d25b3772bb968d1f93
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-08 22:22:20 -0300

    quedan querys como querymethod

--- src/main/java/com/example/gatosspringboot/repository/database/PersonaRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/PersonaRepository.java
@@ -7,9 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface PersonaRepository extends CrudRepository<Persona,Long> {
-    @Query("from Persona p where p.dni= ?1")
+
     Optional<Persona> findByDni(String dni);
 
-    @Query("from Persona p where p.email= ?1")
     Optional<Persona> findByEmail(String email);
 }
