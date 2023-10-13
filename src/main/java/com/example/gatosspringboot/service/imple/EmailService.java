85b2bcdc2460c846746b33e023508ec05eb7d59e
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-13 00:03:00 -0300

    se crea y envía email con formato en email service

--- src/main/java/com/example/gatosspringboot/service/imple/EmailService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/EmailService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 public class EmailService implements IEmailService {
 
     final JavaMailSender emailSender;
-    private Logger logger= LoggerFactory.getLogger(EmailService.class);
+    private Logger logger = LoggerFactory.getLogger(EmailService.class);
 
     public EmailService(JavaMailSender emailSender) {
         this.emailSender = emailSender;
@@ -22,22 +22,33 @@ public class EmailService implements IEmailService {
 
     @Override
     public boolean sendMessage(String to, String subject, String text) {
-        try{
+        try {
             //SimpleMailMessage message=new SimpleMailMessage();
-            MimeMessage message= emailSender.createMimeMessage();
-            MimeMessageHelper helper=new MimeMessageHelper(message,true, "UTF-8");
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setFrom("florencianionquepan@gmail.com");
             helper.setTo(to);
             helper.setSubject(subject);
-            helper.setText(text,true);
+            helper.setText(text, true);
             this.emailSender.send(message);
             return true;
-        }catch (MailException ex){
+        } catch (MailException ex) {
             logger.error(ex.getMessage());
             return false;
         } catch (MessagingException e) {
             logger.error(e.getMessage());
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public boolean armarEnviarEmail(String to, String subject, String text) {
+        String url = "http://localhost:4200/login/";
+        String content = "<div style=\"background-color: #F5CDFF; border-radius: 10px; padding: 10px; font-size: 20px;\">"
+                + "<p>"+text+"</p>"
+                + "<p style=\"margin-bottom: 5px;\">Inicia sesión para obtener más detalles:</p>"
+                + "<a href=\"" + url + "\" style=\"display: inline-block; color: #202124; padding: 10px 20px; text-decoration: none; font-weight: bold; font-size: 16px; background-color: transparent; border: 2px solid #202124; border-radius: 10px;\">Iniciar sesión</a>"
+                + "</div>";
+        return this.sendMessage(to, subject, content);
     }
 }
--- src/main/java/com/example/gatosspringboot/service/interfaces/IEmailService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IEmailService.java
@@ -2,4 +2,5 @@ package com.example.gatosspringboot.service.interfaces;
 
 public interface IEmailService {
     boolean sendMessage(String to, String subject, String text);
+    boolean armarEnviarEmail(String to, String subject,String text);
 }
