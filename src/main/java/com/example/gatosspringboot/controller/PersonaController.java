0e25320b6ebb20c8844fd29f84094a5b305eaa3c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-14 11:32:42 -0300

    endpoint para saber si una persona forma parte de rescats

--- src/main/java/com/example/gatosspringboot/controller/PersonaController.java
+++ src/main/java/com/example/gatosspringboot/controller/PersonaController.java
@@ -2,25 +2,25 @@ package com.example.gatosspringboot.controller;
 
 import com.example.gatosspringboot.dto.PersonaDTO;
 import com.example.gatosspringboot.dto.RegistroDTO;
-import com.example.gatosspringboot.dto.UsuarioEmailDTO;
 import com.example.gatosspringboot.dto.mapper.IPersonaMapper;
 import com.example.gatosspringboot.dto.mapper.IRegistroMapper;
 import com.example.gatosspringboot.dto.mapper.IUsuarioEmailMapper;
 import com.example.gatosspringboot.model.Persona;
 import com.example.gatosspringboot.service.interfaces.IPersonaService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.security.PermitAll;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -53,29 +53,8 @@ public class PersonaController {
         return ResponseEntity.ok(mensajeBody);
     }
 
-    private ResponseEntity<?> notSuccessResponse(String mensaje,Integer id){
-        mensajeBody.put("success",Boolean.FALSE);
-        mensajeBody.put("data", String.format(mensaje,id));
-        return ResponseEntity
-                .badRequest()
-                .body(mensajeBody);
-    }
-
-/*    @GetMapping("/validacion")
-    public ResponseEntity<?> validarEmail(@RequestBody UsuarioEmailDTO dto){
-        String email=this.userMapper.mapToEntity(dto).getEmail();
-        if(this.service.validarEmailIngresado(email)){
-            return this.successResponse("El codigo fue enviado a su email");
-        }else{
-            mensajeBody.put("error",HttpStatus.INTERNAL_SERVER_ERROR.value());
-            mensajeBody.put("data","No se pudo enviar el código al email ingresado."+
-                    "Asegurase que es un email valido o intente nuevamente");
-             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                     .body(mensajeBody);
-        }
-    }*/
-
     @PostMapping
+    @PermitAll
     public ResponseEntity<?> nueva(@RequestBody @Valid RegistroDTO dto){
         Persona nueva=this.service.registro(this.registroMapper.mapToEntity(dto));
         return this.successResponse(this.mapper.mapToDto(nueva));
@@ -97,6 +76,14 @@ public class PersonaController {
                                                      @Email(message="El email no es válido") String email){
         PersonaDTO dto=this.mapper.mapToDto(this.service.findByEmailOrException(email));
         return this.successResponse(dto);
+    }
+
+    @GetMapping("/{dni}/voluntariado")
+    @PreAuthorize("hasAnyRole('SOCIO', 'VOLUNTARIO')")
+    public ResponseEntity<?> esVoluntario(@PathVariable @Pattern(regexp = "\\d{8}",
+            message = "El dni debe contener exactamente 8 números sin puntos") String dni){
+        List<String> data=this.service.tiposVoluntario(dni);
+        return this.successResponse(data);
     }
 
 }
