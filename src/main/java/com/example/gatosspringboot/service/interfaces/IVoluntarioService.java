499406ddceefb0f3276d7d534f989f628050dde5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-23 19:47:25 -0300

    ajustes imports

--- src/main/java/com/example/gatosspringboot/service/interfaces/IVoluntarioService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IVoluntarioService.java
@@ -1,10 +1,8 @@
 package com.example.gatosspringboot.service.interfaces;
 
-import com.example.gatosspringboot.model.Usuario;
 import com.example.gatosspringboot.model.Voluntario;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface IVoluntarioService {
     List<Voluntario> verTodos();
