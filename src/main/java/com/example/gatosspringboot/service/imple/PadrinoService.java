93d12f9c2dceac66d60b30c4914fc3857619cd13
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-23 19:46:12 -0300

    notificación adopción en padrino

--- src/main/java/com/example/gatosspringboot/service/imple/PadrinoService.java
+++ src/main/java/com/example/gatosspringboot/service/imple/PadrinoService.java
@@ -1,9 +1,12 @@
 package com.example.gatosspringboot.service.imple;
 
 import com.example.gatosspringboot.exception.NonExistingException;
+import com.example.gatosspringboot.model.Gato;
+import com.example.gatosspringboot.model.Notificacion;
 import com.example.gatosspringboot.model.Padrino;
-import com.example.gatosspringboot.model.Voluntario;
 import com.example.gatosspringboot.repository.database.PadrinoRepository;
+import com.example.gatosspringboot.service.interfaces.IEmailService;
+import com.example.gatosspringboot.service.interfaces.INotificacionService;
 import com.example.gatosspringboot.service.interfaces.IPadrinoService;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +15,16 @@ import java.util.Optional;
 @Service
 public class PadrinoService implements IPadrinoService {
 
-    public final PadrinoRepository repo;
+    private final PadrinoRepository repo;
+    private final INotificacionService notiService;
+    private final IEmailService emailService;
 
-    public PadrinoService(PadrinoRepository repo) {
+    public PadrinoService(PadrinoRepository repo,
+                          INotificacionService notiService,
+                          IEmailService emailService) {
         this.repo = repo;
+        this.notiService = notiService;
+        this.emailService = emailService;
     }
 
     @Override
@@ -28,5 +37,12 @@ public class PadrinoService implements IPadrinoService {
             );
         }
         return oPadri.get();
+    }
+
+    @Override
+    public void notificarAdopcion(Padrino padrino, Gato gato) {
+        Notificacion noti=this.notiService.notificarAdopcion(padrino,gato);
+        String subject=gato.getNombre()+" fue adoptado!";
+        this.emailService.armarEnviarEmail(padrino.getPersona().getEmail(),subject,noti.getDescripcion());
     }
 }
--- src/main/java/com/example/gatosspringboot/service/interfaces/IPadrinoService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IPadrinoService.java
@@ -1,7 +1,9 @@
 package com.example.gatosspringboot.service.interfaces;
 
+import com.example.gatosspringboot.model.Gato;
 import com.example.gatosspringboot.model.Padrino;
 
 public interface IPadrinoService {
     Padrino buscarByEmailOrException(String email);
+    void notificarAdopcion(Padrino padrino, Gato gato);
 }
