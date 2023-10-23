e952f7e579a8ec843e622c02e317f81d1ebbff26
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-22 21:03:23 -0300

    agrego endpoint para verGatos de transito

--- src/main/java/com/example/gatosspringboot/controller/TransitoController.java
+++ src/main/java/com/example/gatosspringboot/controller/TransitoController.java
@@ -1,9 +1,13 @@
 package com.example.gatosspringboot.controller;
 
+import com.example.gatosspringboot.dto.GatoRespDTO;
+import com.example.gatosspringboot.dto.mapper.IGatoMapper;
 import com.example.gatosspringboot.dto.mapper.ITransitoMapper;
+import com.example.gatosspringboot.model.Gato;
 import com.example.gatosspringboot.model.Transito;
 import com.example.gatosspringboot.service.interfaces.ITransitoService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -16,12 +20,15 @@ public class TransitoController {
 
     private final ITransitoService service;
     private final ITransitoMapper mapper;
+    private final IGatoMapper gatoMapper;
     public Map<String,Object> mensajeBody= new HashMap<>();
 
     public TransitoController(ITransitoService service,
-                              ITransitoMapper mapper) {
+                              ITransitoMapper mapper,
+                              IGatoMapper gatoMapper) {
         this.service = service;
         this.mapper = mapper;
+        this.gatoMapper = gatoMapper;
     }
 
     private ResponseEntity<?> successResponse(Object data){
@@ -30,24 +37,25 @@ public class TransitoController {
         return ResponseEntity.ok(mensajeBody);
     }
 
-    private ResponseEntity<?> notSuccessResponse(String mensaje,int id){
-        mensajeBody.put("success",Boolean.FALSE);
-        mensajeBody.put("data", String.format(mensaje,id));
-        return ResponseEntity
-                .badRequest()
-                .body(mensajeBody);
-    }
-
     @GetMapping
+    @PreAuthorize("hasRole('VOLUNTARIO')")
     public ResponseEntity<?> listarTodos(){
         List<Transito> transitos=this.service.listarTodos();
         return this.successResponse(this.mapper.mapToListDto(transitos));
     }
 
     @GetMapping("/localidad/{localidad}")
+    @PreAuthorize("hasRole('VOLUNTARIO')")
     public ResponseEntity<?> listarByLocalidad(@PathVariable String localidad){
         List<Transito> transitos=this.service.listarByLocalidad(localidad);
         return this.successResponse(this.mapper.mapToListDto(transitos));
+    }
+
+    @GetMapping("/{email}/gatos")
+    @PreAuthorize("#email==authentication.principal")
+    public ResponseEntity<?> verGatos(@PathVariable String email){
+        List<Gato> gatos=this.service.listarGatos(email);
+        return this.successResponse(this.gatoMapper.mapListToDto(gatos));
     }
 
 }
