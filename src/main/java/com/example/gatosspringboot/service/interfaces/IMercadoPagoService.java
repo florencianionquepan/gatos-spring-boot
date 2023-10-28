6bcfc44320b8dd5198f931886a9823d6d39ee27d
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-28 19:50:46 -0300

    Update IMercadoPagoService.java

--- src/main/java/com/example/gatosspringboot/service/interfaces/IMercadoPagoService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/IMercadoPagoService.java
@@ -1,10 +1,11 @@
 package com.example.gatosspringboot.service.interfaces;
 
-import com.example.gatosspringboot.model.Gato;
-import com.example.gatosspringboot.model.Padrino;
+import com.example.gatosspringboot.model.Cuota;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 
 public interface IMercadoPagoService {
-    void crearPago(Gato gato) throws MPException, MPApiException;
+    void crearPago(Cuota cuota) throws MPException, MPApiException;
+
+    String crearPreferencia(Cuota cuota) throws MPException, MPApiException;
 }
