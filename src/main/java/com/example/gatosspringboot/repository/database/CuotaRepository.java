2fcf5f9eda3f85d33e79ec6734518381329e791f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-29 19:30:14 -0300

    Update CuotaRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/CuotaRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/CuotaRepository.java
@@ -4,4 +4,6 @@ import com.example.gatosspringboot.model.Cuota;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CuotaRepository extends CrudRepository<Cuota,Long> {
+
+    Cuota findByPreferencia_id(String preferencia_id);
 }
