099449095402716cf32c8008b46df4a7b65261ea
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-29 20:41:30 -0300

    Update CuotaRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/CuotaRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/CuotaRepository.java
@@ -1,9 +1,11 @@
 package com.example.gatosspringboot.repository.database;
 
 import com.example.gatosspringboot.model.Cuota;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CuotaRepository extends CrudRepository<Cuota,Long> {
 
+    @Query("SELECT c FROM Cuota c WHERE c.preferencia_id = ?1")
     Cuota findByPreferencia_id(String preferencia_id);
 }
