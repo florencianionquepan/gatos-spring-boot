59177040ce321fe1725b3c4aeb147f388486313c
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-10-31 22:53:28 -0300

    Create PadrinoController.java

---
+++ src/main/java/com/example/gatosspringboot/controller/PadrinoController.java
@@ -0,0 +1,42 @@
+package com.example.gatosspringboot.controller;
+
+import com.example.gatosspringboot.dto.GatoIdDTO;
+import com.example.gatosspringboot.dto.mapper.IGatoIdMapper;
+import com.example.gatosspringboot.model.Padrino;
+import com.example.gatosspringboot.service.interfaces.IPadrinoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/padrinos")
+public class PadrinoController {
+
+    private final IPadrinoService service;
+    private final IGatoIdMapper gatoMapper;
+
+    public Map<String,Object> mensajeBody= new HashMap<>();
+
+    public PadrinoController(IPadrinoService service,
+                             IGatoIdMapper gatoMapper) {
+        this.service = service;
+        this.gatoMapper = gatoMapper;
+    }
+
+    private ResponseEntity<?> successResponse(Object data){
+        mensajeBody.put("success",Boolean.TRUE);
+        mensajeBody.put("data",data);
+        return ResponseEntity.ok(mensajeBody);
+    }
+
+    @PutMapping("/{email}")
+    @PreAuthorize("#email==authentication.principal")
+    //este endpoint es para que el propio padrino remueva un gato
+    public ResponseEntity<?> removerGato(@PathVariable String email, @RequestBody GatoIdDTO dto){
+        Padrino modi=this.service.removerGato(email,this.gatoMapper.mapToEntity(dto));
+        return this.successResponse(modi);
+    }
+}
