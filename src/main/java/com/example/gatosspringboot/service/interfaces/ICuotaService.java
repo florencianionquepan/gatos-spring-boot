6160551c4c6f5f2382d202215c107acbc751217a
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-04 16:41:05 -0300

    Update ICuotaService.java

--- src/main/java/com/example/gatosspringboot/service/interfaces/ICuotaService.java
+++ src/main/java/com/example/gatosspringboot/service/interfaces/ICuotaService.java
@@ -7,8 +7,9 @@ import java.util.List;
 public interface ICuotaService {
 
     List<Cuota> listarByPadrino(String email);
-    String creacionPreferencia(Cuota cuota);
+    String creacionPreferenciaPrimeraCuota(Cuota cuota);
     Cuota creacionCuota(Cuota cuota);
+    String creacionPreferencia(Long idCuota);
     Cuota pagoCuotaAprobado(String preferenciaId);
     Cuota pagoCuotaRechazado(String preferenciaId);
 }
