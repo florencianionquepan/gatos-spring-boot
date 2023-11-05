416606bff435b8fbe00205c795b4b33688fedb4b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-05 15:08:19 -0300

    Create GatoTransitoRepository.java

---
+++ src/main/java/com/example/gatosspringboot/repository/database/GatoTransitoRepository.java
@@ -0,0 +1,7 @@
+package com.example.gatosspringboot.repository.database;
+
+import com.example.gatosspringboot.model.GatoTransito;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GatoTransitoRepository extends CrudRepository<GatoTransito,Long> {
+}
