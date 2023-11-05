b0caf1d3fb91838112ccc2b1c9a1f6011f99dd57
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-05 17:09:13 -0300

    Update GatoTransitoRepository.java

--- src/main/java/com/example/gatosspringboot/repository/database/GatoTransitoRepository.java
+++ src/main/java/com/example/gatosspringboot/repository/database/GatoTransitoRepository.java
@@ -1,7 +1,19 @@
 package com.example.gatosspringboot.repository.database;
 
+import com.example.gatosspringboot.model.Gato;
 import com.example.gatosspringboot.model.GatoTransito;
+import com.example.gatosspringboot.model.Transito;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GatoTransitoRepository extends CrudRepository<GatoTransito,Long> {
+
+    @Query("SELECT gt FROM GatoTransito gt WHERE gt.gato = ?1 AND gt.transito = ?2 ORDER BY gt.fechaAsociacion DESC")
+    GatoTransito findByGatoAndTransito(Gato gato, Transito transito);
+
+    List<GatoTransito> findByTransito(Transito transito);
+
+    List<GatoTransito> findByGato(Gato gato);
 }
