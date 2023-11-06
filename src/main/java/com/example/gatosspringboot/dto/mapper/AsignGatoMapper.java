b980cf66ec7bcc7655a8c902bebee8c4385440f4
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-05 21:40:17 -0300

    agrego mapper para asignaciones transitos

--- src/main/java/com/example/gatosspringboot/dto/mapper/AsignGatoMapper.java
+++ src/main/java/com/example/gatosspringboot/dto/mapper/AsignGatoMapper.java
@@ -1,6 +1,7 @@
 package com.example.gatosspringboot.dto.mapper;
 
 import com.example.gatosspringboot.dto.AsignacionGatoDTO;
+import com.example.gatosspringboot.dto.AsignacionTransitoDTO;
 import com.example.gatosspringboot.model.GatoTransito;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +12,12 @@ import java.util.stream.Collectors;
 public class AsignGatoMapper implements IAsignGatoMapper{
 
     private final IGatoMapper gatoMap;
+    private final IPersonaMapper persoMap;
 
-    public AsignGatoMapper(IGatoMapper gatoMap) {
+    public AsignGatoMapper(IGatoMapper gatoMap,
+                           IPersonaMapper persoMap) {
         this.gatoMap = gatoMap;
+        this.persoMap = persoMap;
     }
 
     @Override
@@ -29,9 +33,28 @@ public class AsignGatoMapper implements IAsignGatoMapper{
     }
 
     @Override
+    public AsignacionTransitoDTO mapToDtoTransito(GatoTransito entity) {
+        AsignacionTransitoDTO dto=new AsignacionTransitoDTO();
+        dto.setId(entity.getId());
+        dto.setFechaAsignacion(entity.getFechaAsociacion());
+        if(entity.getFechaFin()!=null) {
+            dto.setFechaFin(entity.getFechaFin());
+        }
+        dto.setTransito(this.persoMap.mapToDto(entity.getTransito().getPersona()));
+        return dto;
+    }
+
+    @Override
     public List<AsignacionGatoDTO> mapToListDto(List<GatoTransito> asignacionesGatos) {
         return asignacionesGatos.stream()
                 .map(this::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<AsignacionTransitoDTO> mapToListDtoTransito(List<GatoTransito> asignaciones) {
+        return asignaciones.stream()
+                .map(this::mapToDtoTransito)
                 .collect(Collectors.toList());
     }
 }
--- src/main/java/com/example/gatosspringboot/dto/mapper/IAsignGatoMapper.java
+++ src/main/java/com/example/gatosspringboot/dto/mapper/IAsignGatoMapper.java
@@ -1,11 +1,14 @@
 package com.example.gatosspringboot.dto.mapper;
 
 import com.example.gatosspringboot.dto.AsignacionGatoDTO;
+import com.example.gatosspringboot.dto.AsignacionTransitoDTO;
 import com.example.gatosspringboot.model.GatoTransito;
 
 import java.util.List;
 
 public interface IAsignGatoMapper {
     AsignacionGatoDTO mapToDto(GatoTransito entity);
+    AsignacionTransitoDTO mapToDtoTransito(GatoTransito entity);
     List<AsignacionGatoDTO> mapToListDto(List<GatoTransito> asignacionesGatos);
+    List<AsignacionTransitoDTO> mapToListDtoTransito(List<GatoTransito> asignaciones);
 }
