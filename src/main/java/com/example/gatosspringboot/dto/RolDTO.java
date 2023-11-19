14be977b18afad0620ee0d86b4cd9407245b76d9
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2023-11-18 21:22:00 -0300

    Create RolDTO.java

---
+++ src/main/java/com/example/gatosspringboot/dto/RolDTO.java
@@ -0,0 +1,13 @@
+package com.example.gatosspringboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RolDTO {
+    private int id;
+    private String nombre;
+}
